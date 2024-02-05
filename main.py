2c96c8a0d58ebe8b4d9a4e24774acac71e0b7127
DoğukanKorkmaz <dkorkmaz2000@hotmail.com>
2024-02-05 21:52:24 +0300

    Pandas added. Data configured. Prompt added and modified.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -6,11 +7,18 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+data = pandas.read_csv("50_states.csv")
+states = data["state"]
+states.to_list()
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
-print(answer_state)
+game_is_on = True
 
-
-
+while game_is_on:
+    answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+    if answer_state is None:
+        screen.bye()
+    for x in states:
+        if x == answer_state.capitalize():
+            print("bum")
 
 turtle.mainloop()
